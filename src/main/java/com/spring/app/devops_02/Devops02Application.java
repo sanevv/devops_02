f8e09f08eca5b53552221bb38c8453231813b2af
sanevv <sanevv@naver.com>
2025-09-02 11:29:31 +0900

    docker first commit

---
+++ src/main/java/com/spring/app/devops_02/Devops02Application.java
@@ -0,0 +1,13 @@
+package com.spring.app.devops_02;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Devops02Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Devops02Application.class, args);
+    }
+
+}
---
+++ src/test/java/com/spring/app/devops_02/Devops02ApplicationTests.java
@@ -0,0 +1,13 @@
+package com.spring.app.devops_02;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Devops02ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
